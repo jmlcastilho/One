5a95bc758dfe7eb99ba697f0c52cfbb4bd709b99
juliano <jmlcastilho@gmail.com>
2021-09-19 20:02:06 -0300

    CRUD Scaffolding

---
+++ VendasMvc/Data/VendasMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendasMvc.Models
+{
+    public class VendasMvcContext : DbContext
+    {
+        public VendasMvcContext (DbContextOptions<VendasMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasMvc.Models.Department> Department { get; set; }
+    }
+}
