0b002aaf07a235d79bf1e2e99170507a112305e2
juliano <jmlcastilho@gmail.com>
2021-09-19 19:15:33 -0300

    Deleting Department view and controller

---
+++ VendasMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic; /*Usado para o List*/
+using System.Linq;
+using System.Threading.Tasks;
+using VendasMvc.Models; /*Reconhecer o Departamento*/
+
+namespace VendasMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()  /*Criar a view com o mesmo nome para casar a ação*/
+        {
+            List<Departamento> list = new List<Departamento>(); /*Dar o nome de List e instanciar Departamento*/
+            list.Add(new Departamento { Id = 1, Name = "Eletronico" });
+            list.Add(new Departamento { Id = 2, Name = "Hidraulico" });
+
+            return View(list); /*Enviar os dados da lista para a View- dados do controller para a view*/
+        }
+    }
+}
---
+++ VendasMvc/Controllers/TestesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasMvc.Models;
+
+namespace VendasMvc.Controllers
+{
+    public class TestesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Teste> list = new List<Teste>();
+            list.Add(new Teste { Id = 1, Name = "Elemento 1" });
+            list.Add(new Teste { Id = 2, Name = "Elemento 2" });
+            return View(list);
+        }
+    }
+}
